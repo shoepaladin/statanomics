a063bb42c0140274723430d288d3d0fbd3eb94db
shoepaladin <77134137+shoepaladin@users.noreply.github.com>
2021-12-23 15:08:35 -0800

    pip installability v1

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='my_pip_package',
+    version='0.0.5',
+    version=__version__,
+
+    url='https://github.com/shoepaladin/statanomics/',
+    author='Julian Hsu',
+    author_email='tarobum27@gmail.com',
+    license='MIT',
+    py_modules=['my_pip_package'],
+    install_requires=['numpy','stats models','scipy'],
+)
