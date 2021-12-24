9fde5f91a6643f118b9f44859fa1fa6824beafb5
shoepaladin <77134137+shoepaladin@users.noreply.github.com>
2021-12-24 07:12:15 -0800

    pip v1

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from my_pip_package import __version__
 setup(
     name='my_pip_package',
     version='0.0.5',
-    version=__version__,
 
     url='https://github.com/shoepaladin/statanomics/',
     author='Julian Hsu',
     author_email='tarobum27@gmail.com',
     license='MIT',
     py_modules=['my_pip_package','ATELibrary'],
-    install_requires=['numpy','statsmodels','spicy','economy'],
+    install_requires=['numpy','statsmodels','scipy','econml'],
 )
