db9e0990296333548d2b7263c5dbc0109cc28257
shoepaladin <77134137+shoepaladin@users.noreply.github.com>
2021-12-24 12:45:55 -0800

    Write Sample Notebooks
    Writing sample notebooks using diagnostics, ate, and hte classes.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
-from my_pip_package import __version__
-
 setup(
     name='stnomics',
     version='0.0.5',
     url='https://github.com/shoepaladin/statanomics/',
     author='Julian Hsu',
     author_email='tarobum27@gmail.com',
+    description='Installation of the Statanomics package. Thank you for using!',
     license='MIT',
     py_modules=['stnomics'],
     install_requires=['numpy','statsmodels','econml','scipy'],
