9fb2eeb6a7dcbba077ccb641f7760bd406126354
shoepaladin <77134137+shoepaladin@users.noreply.github.com>
2021-12-24 07:36:24 -0800

    pip troubleshooting
     changing location of my_pip_package to the causalmodels folder

--- causalmodels/setup.py
+++ causalmodels/setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
-setup(
-    name='atelibrary',
-    version='0.0.5',
+from my_pip_package import __version__
 
-    url='https://github.com/shoepaladin/statanomics/causalmodels',
+setup(
+    name='my_pip_package',
+    version='0.0.5',
+    url='https://github.com/shoepaladin/statanomics/',
     author='Julian Hsu',
     author_email='tarobum27@gmail.com',
     license='MIT',
-    py_modules=['atelibrary'],
-    install_requires=['numpy','statsmodels','scipy','econml],
+    py_modules=['my_pip_package','ATELibrary'],
+    install_requires=['numpy','statsmodels'],
 )
