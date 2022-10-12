60e76993be770d62ed9f82074439936e6ed87138
Julian Hsu <hsujulia@amazon.com>
2022-10-12 15:31:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stnomics',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/shoepaladin/statanomics/',
     author='Julian Hsu',
     author_email='tarobum27@gmail.com',
